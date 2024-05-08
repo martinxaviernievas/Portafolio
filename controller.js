9173f9eb2830627e442ca9e79d042e7103c82acc
MartinXavierNievas <100978531+martinxaviernievas@users.noreply.github.com>
2024-05-08 19:30:23 -0300

    Finalizado

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@ window.onload = () => {
   const navMenu = document.querySelector('.nav-menu');
   const navItems = document.querySelectorAll('.nav-item');
   const hamburger = document.querySelector('.nav-toggle');
+  const formulario = document.querySelector();
   
   const toggle = e => e.classList.toggle('is-active');
   const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
